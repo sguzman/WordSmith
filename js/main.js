dfcaa5b30db3c465159336429f59a9fee7e0c34b
Salvador Guzman <guzmansalv@gmail.com>
2017-07-02 23:56:04 -0700

    added remove on double click function use jquery to copy and change id for new element from letter holder overwrite any child target elememtn

--- js/main.js
+++ js/main.js
@@ -10,9 +10,23 @@ function drop(ev) {
     ev.preventDefault();
     const data = ev.dataTransfer.getData("text");
     const source = document.getElementById(data);
+    if (ev.target.tagName !== 'TD') {
+        return;
+    }
+
+    if (ev.target.childElementCount > 0) {
+        ev.target.removeChild(ev.target.children[0]);
+    }
+
     if (source.parentNode.tagName === 'TD') {
         ev.target.appendChild(source);
     } else {
-        ev.target.appendChild(source.cloneNode(true));
+        $(`#${data}`).clone().attr('id', `${source.id}-${ev.target.id}`).appendTo(ev.target);
+    }
+}
+
+function removeOnDblClick(ev) {
+    if (ev.target.parentNode.tagName === 'TD') {
+        ev.target.parentNode.removeChild(ev.target);
     }
 }
